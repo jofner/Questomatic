87c4353a162edc0499c256491bef7ce97e7bddcb
risky <info@boot.cz>
2010-03-08 14:10:19 +0100

    Added French translation by ckeurk

---
+++ locales/frFR.lua
@@ -0,0 +1,18 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Questomatic", "frFR")  
+if not L then return end
+----French localization by ckeurk
+
+L["AddOn Enable"] = "AddOn Activer"
+L["Enable/Disable Quest-o-matic"] = "Activer/Désactiver Quest-o-matic"
+L["Auto Accept Quests"] = "Auto accepte les Quêtes"
+L["Enable/Disable auto quest accepting"] = "Activer/Désactiver une acceptation automatique des quêtes"
+L["Skip Greetings"] = "Passer les explications"
+L["Enable/Disable NPC's greetings skip for one or more quests"] = "Activer / Désactiver sauter les explications du pnj pour une ou plusieurs quêtes"
+L["Auto Accept Escorts"] = "Auto accepte l’escorte"
+L["Enable/Disable auto escort accepting"] = "Activer/Désactiver l’acceptation Auto d’escorte"
+L["Auto Complete Quests"] = "Auto fin des Quêtes"
+L["Enable/Disable auto quest complete"] = "Activer/Désactiver la validation automatique des quêtes finies"
+L["Auto Accept in Raid"] = "Auto accepte raid"
+L["Enable/Disable auto accepting quests in raid"] = "Activer/Désactiver une acceptation automatique pour une quête de raid"
+L["Config"] = "Config"
+L["Open configuration"] = "Ouvrir la configuration"
