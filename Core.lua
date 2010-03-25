82a38cdc5d1717deb9caa23af1535910a817e79b
risky <info@boot.cz>
2010-03-25 08:46:00 +0100

    Global settings profile was changed to character specific

--- Core.lua
+++ Core.lua
@@ -11,48 +11,48 @@ local options = {
             type = "toggle",
             name = L["AddOn Enable"],
             desc = L["Enable/Disable Quest-o-matic"],
-            get = function() return Questomatic.db.profile.toggle end,
-            set = function( info, value ) Questomatic.db.profile.toggle = value end
+            get = function() return Questomatic.db.char.toggle end,
+            set = function( info, value ) Questomatic.db.char.toggle = value end
         },
         accept = {
             order = 2,
             type = "toggle",
             name = L["Auto Accept Quests"],
             desc = L["Enable/Disable auto quest accepting"],
-            get = function() return Questomatic.db.profile.accept end,
-            set = function( info, value ) Questomatic.db.profile.accept = value end
+            get = function() return Questomatic.db.char.accept end,
+            set = function( info, value ) Questomatic.db.char.accept = value end
         },
         greeting = {
             order = 3,
             type = "toggle",
             name = L["Skip Greetings"],
             desc = L["Enable/Disable NPC's greetings skip for one or more quests"],
-            get = function() return Questomatic.db.profile.greeting end,
-            set = function( info, value ) Questomatic.db.profile.greeting = value end
+            get = function() return Questomatic.db.char.greeting end,
+            set = function( info, value ) Questomatic.db.char.greeting = value end
         },
         escort = {
             order = 4,
             type = "toggle",
             name = L["Auto Accept Escorts"],
             desc = L["Enable/Disable auto escort accepting"],
-            get = function() return Questomatic.db.profile.escort end,
-            set = function( info, value) Questomatic.db.profile.escort = value end
+            get = function() return Questomatic.db.char.escort end,
+            set = function( info, value) Questomatic.db.char.escort = value end
         },
         complete = {
             order = 5,
             type = "toggle",
             name = L["Auto Complete Quests"],
             desc = L["Enable/Disable auto quest complete"],
-            get = function() return Questomatic.db.profile.complete end,
-            set = function( info, value ) Questomatic.db.profile.complete = value end
+            get = function() return Questomatic.db.char.complete end,
+            set = function( info, value ) Questomatic.db.char.complete = value end
         },
         inraid = {
             order = 6,
             type = "toggle",
             name = L["Auto Accept in Raid"],
             desc = L["Enable/Disable auto accepting quests in raid"],
-            get = function() return Questomatic.db.profile.inraid end,
-            set = function( info, value ) Questomatic.db.profile.inraid = value end
+            get = function() return Questomatic.db.char.inraid end,
+            set = function( info, value ) Questomatic.db.char.inraid = value end
         },
         config = {
             order = 7,
@@ -66,7 +66,7 @@ local options = {
 }
 
 local defaults = {
-    profile = {
+    char = {
         toggle = true,
         accept = true,
         greeting = true,
@@ -77,7 +77,7 @@ local defaults = {
 }
 
 function Questomatic:OnInitialize()
-    self.db = LibStub("AceDB-3.0"):New("QOMDB", defaults, "Default");
+    self.db = LibStub("AceDB-3.0"):New("QOMDB", defaults);
     LibStub("AceConfig-3.0"):RegisterOptionsTable("Questomatic", options, {"Questomatic", "qm"});
     LibStub("AceConfigDialog-3.0"):AddToBlizOptions("Questomatic", "Questomatic");
 end
@@ -90,21 +90,21 @@ function Questomatic:OnEnable()
     self:RegisterEvent("QUEST_PROGRESS")
     self:RegisterEvent("QUEST_COMPLETE")
     
-    self.db.profile.toggle = true
+    self.db.char.toggle = true
 end
 
 function Questomatic:OnDisable()
     self:UnregisterAllEvents()
     
-    self.db.profile.toggle = false
+    self.db.char.toggle = false
 end
 
 function Questomatic:QUEST_GREETING(eventName, ...)
-    if UnitInRaid("player") and ( not self.db.profile.inraid ) then
+    if UnitInRaid("player") and ( not self.db.char.inraid ) then
         return;
     end
     
-    if (self.db.profile.toggle) and (self.db.profile.greeting) and ( not IsControlKeyDown() ) then
+    if (self.db.char.toggle) and (self.db.char.greeting) and ( not IsControlKeyDown() ) then
         local numact,numava = GetNumActiveQuests(), GetNumAvailableQuests()
         if numact+numava == 0 then return end
 
@@ -118,11 +118,11 @@ function Questomatic:QUEST_GREETING(eventName, ...)
 end
 
 function Questomatic:GOSSIP_SHOW(eventName, ...)
-    if UnitInRaid("player") and ( not self.db.profile.inraid ) then
+    if UnitInRaid("player") and ( not self.db.char.inraid ) then
         return;
     end
     
-    if (self.db.profile.toggle) and (self.db.profile.greeting) and ( not IsControlKeyDown() ) then
+    if (self.db.char.toggle) and (self.db.char.greeting) and ( not IsControlKeyDown() ) then
         if GetGossipAvailableQuests() then
             SelectGossipAvailableQuest(1);
         elseif GetGossipActiveQuests() then
@@ -132,33 +132,33 @@ function Questomatic:GOSSIP_SHOW(eventName, ...)
 end
 
 function Questomatic:QUEST_DETAIL(eventName, ...)
-    if UnitInRaid("player") and ( not self.db.profile.inraid ) then
+    if UnitInRaid("player") and ( not self.db.char.inraid ) then
         return;
     end
     
-    if (self.db.profile.toggle) and (self.db.profile.accept) and ( not IsControlKeyDown() ) then
+    if (self.db.char.toggle) and (self.db.char.accept) and ( not IsControlKeyDown() ) then
         AcceptQuest();
     end
 end
 
 function Questomatic:QUEST_ACCEPT_CONFIRM(eventName, ...)
-    if UnitInRaid("player") and ( not self.db.profile.inraid ) then
+    if UnitInRaid("player") and ( not self.db.char.inraid ) then
         return;
     end
     
-    if (self.db.profile.toggle) and (self.db.profile.escort) and ( not IsControlKeyDown() ) then
+    if (self.db.char.toggle) and (self.db.char.escort) and ( not IsControlKeyDown() ) then
         ConfirmAcceptQuest();
     end
 end
 
 function Questomatic:QUEST_PROGRESS(eventName, ...)
-    if (self.db.profile.toggle) and (self.db.profile.complete) and ( not IsControlKeyDown() ) then
+    if (self.db.char.toggle) and (self.db.char.complete) and ( not IsControlKeyDown() ) then
         CompleteQuest();
     end
 end
 
 function Questomatic:QUEST_COMPLETE(eventName, ...)
-    if (self.db.profile.toggle) and (self.db.profile.complete) and ( not IsControlKeyDown() ) then
+    if (self.db.char.toggle) and (self.db.char.complete) and ( not IsControlKeyDown() ) then
         if GetNumQuestChoices() == 0 then
             GetQuestReward(QuestFrameRewardPanel.itemChoice);
         end
