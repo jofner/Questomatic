e3154a1e3959803593e25c76cdece664e99bc361
risky <info@boot.cz>
2011-04-14 23:46:03 +0200

    update zhCN and zhTW locale by networm

---
+++ locales/zhCN.lua
@@ -0,0 +1,19 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Questomatic", "zhCN", false)
+-- Simplified Chinese Localization by networm(狂飙)
+
+if not L then return end
+
+L["AddOn Enable"] = "启用插件"
+L["Enable/Disable Quest-o-matic"] = "启用/停用 Questomatic"
+L["Auto Accept Quests"] = "自动接受任务"
+L["Enable/Disable auto quest accepting"] = "启用/停用 自动接受任务"
+L["Skip Greetings"] = "跳过闲聊窗口"
+L["Enable/Disable NPC's greetings skip for one or more quests"] = "启用/停用 跳过NPC闲聊窗口"
+L["Auto Accept Escorts"] = "自动接受护送任务"
+L["Enable/Disable auto escort accepting"] = "启用/停用 自动接受护送任务"
+L["Auto Complete Quests"] = "自动完成任务"
+L["Enable/Disable auto quest complete"] = "启用/停用 自动完成任务"
+L["Auto Accept in Raid"] = "自动接受团队任务"
+L["Enable/Disable auto accepting quests in raid"] = "启用/停用 自动接受团队任务"
+L["Config"] = "配置"
+L["Open configuration"] = "打开配置界面"
